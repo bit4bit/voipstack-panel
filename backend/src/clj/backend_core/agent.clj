26ff10fbda223ab6ded53438d3fbe376f2df5713
bit4bit <bit4bit@riseup.net>
2022-05-30 23:48:17 +0000

    improv
    FossilOrigin-Name: d0a9965405e1cce3b9d2fac53c9b966ec59fc7f95550d3165c243ce7b20e4da6

--- backend/src/clj/backend_core/agent.clj
+++ backend/src/clj/backend_core/agent.clj
@@ -2,14 +2,13 @@
   (:require
    [backend-core.state :as state]))
 
-
+;; abstraction of agent runner
 (defprotocol core-runner
   (start [this] "start runner")
   (stop [this] "stop current runner")
   (dispatch [this agent-id content] "returns dispatcher"))
 
-
-;; first implementation of core
+;; first implementation of core, we do calls in same thread
 ;; for high traffic maybe we new a decoupled implementation core
 (deftype core-runner-sync [send-fn]
   core-runner
@@ -18,17 +17,18 @@
   (dispatch [this agent-id event]
       (send-fn agent-id [:event/state event])))
 
+;; constructor of core-runner-sync
 (defn new-core-runner-sync [send-fn]
   (core-runner-sync. send-fn))
 
-(defn run-dispatcher [runner]
+(defn- call-dispatcher [runner]
   (fn [id event]
     (state/validate event)
     (dispatch runner id event)))
 
 (defn start! [runner]
   "start agent handler"
-  {:dispatch-fn (run-dispatcher runner)
+  {:dispatch-fn (call-dispatcher runner)
    :stop-fn
    (fn []
      (stop runner))})
