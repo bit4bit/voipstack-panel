1296dc3eb8bfb4fc540b1256297975467ead2e9d
bit4bit <bit4bit@riseup.net>
2022-07-02 03:14:56 +0000

    agent: check spec of runtime state
    FossilOrigin-Name: 29a404db6c01e894591f62744f9ee3b4c52afc4c29781759b5bbff4f4e9d82fd

--- agent/src/voipstack/agent/runtime.clj
+++ agent/src/voipstack/agent/runtime.clj
@@ -4,21 +4,42 @@
   keep and update the state of softswitch.
   "
   (:require
-   [sci.core :as sci]))
+   [sci.core :as sci]
+   [clojure.spec.alpha :as s]))
 
-(defn string->new [code]
-  (let [initial-state {}
+(s/def ::source keyword?)
+(s/def ::id string?)
+(s/def ::alias string?)
+(s/def ::source-endpoint string?)
+(s/def ::source-id string?)
+(s/def ::destination-endpoint string?)
+(s/def ::destination-id string?)
+(s/def ::extension (s/keys :req-un [::id]
+                           :opt-un [::alias]))
+(s/def ::extensions (s/map-of string? ::extension))
+(s/def ::call (s/keys :req-un [::source-endpoint
+                               ::source-id
+                               ::destination-endpoint
+                               ::destination-id]))
+(s/def ::calls (s/* ::call))
+(s/def ::state (s/keys :req-un [::source]
+                       :opt-un [::extensions ::calls]))
+
+(defn string->new [source code]
+  (let [initial-state {:source source}
         var-event (sci/new-dynamic-var 'event {})
         var-source (sci/new-dynamic-var 'source {})
         ;; https://github.com/babashka/sci
         ctx (sci/init {:bindings {'state initial-state 'event var-event 'source var-source}})]
     (sci/eval-string* ctx code)
     {:context ctx
+     :source source
      :state initial-state
      :vars {:event var-event :source var-source}}))
 
-(defn process-event [runtime source event]
+(defn process-event [runtime event]
   (let [ctx (:context runtime)
+        source (:source runtime)
         runtime-var-event (get-in runtime [:vars :event])
         runtime-var-source (get-in runtime [:vars :source])]
     
@@ -28,5 +49,5 @@
        runtime
        {:state (sci/eval-string* ctx "(process-event source state event)")}))))
 
-(defn state [ctx]
+(defn get-state [ctx]
   (:state ctx))
--- agent/test/voipstack/agent_test.clj
+++ agent/test/voipstack/agent_test.clj
@@ -1,14 +1,29 @@
 (ns voipstack.agent-test
   (:require
    [clojure.test :refer :all]
+   [clojure.spec.alpha :as s]
    [voipstack.agent.runtime :as runtime]))
 
 (deftest runtime
   (testing "update state on softswitch event"
-    (let [rt (runtime/string->new "
+    (let [rt (runtime/string->new :test "
 (defn process-event [source state event]
-  (merge state {:test \"from scripting\"}))
+  (merge state {:test (str \"from scripting \" (:event-name event))}))
 ")
-          rt (runtime/process-event rt :test {})]
-      (is (= "from scripting" (:test (runtime/state rt)))))))
+          rt (runtime/process-event rt {:event-name "voipstack"})]
+      
+      (is (= "from scripting voipstack" (:test (runtime/get-state rt))))))
+ (testing "spec runtime state"
+   (let [rt (runtime/string->new :test "")
+         state {:source :test
+                :extensions {"1000" {:id "1000"}}
+                :calls [
+                        {:source-endpoint "mod_sofia"
+                         :source-id "sip-profile"
+                         :destination-endpoint "mod_sofia"
+                         :destination-id "1000"
+                        }]}]
+      
+      (is (= true (s/valid? ::runtime/state state))))))
+
 
